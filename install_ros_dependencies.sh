bd79936f36c90e02d29ec96d0f473db884327fe4
07hokage <saihaneesh.allu@gmail.com>
2024-10-02 15:58:14 -0500

    dependencies

---
+++ install_ros_dependencies.sh
@@ -0,0 +1,15 @@
+#General dependencies
+sudo apt install -y ros-noetic-tf2-sensor-msgs ros-noetic-gazebo-ros-pkgs python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential python3-rosdep
+
+#Navigation Dependencies
+sudo apt install -y ros-noetic-navigation ros-noetic-move-base ros-noetic-slam-gmapping ros-noetic-grid-map-costmap-2d ros-noetic-teleop-twist-joy ros-noetic-pointcloud-to-laserscan ros-noetic-rosbridge-server
+
+# Control system Dependencies
+sudo apt install -y ros-noetic-robot-controllers ros-noetic-gazebo-ros-control
+
+# Perception Dependencies
+sudo apt install -y ros-noetic-rgbd-launch
+
+# Manipulation Dependencies
+sudo apt install -y ros-noetic-moveit ros-noetic-trac-ik ros-noetic-moveit-python
+
